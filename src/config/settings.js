72cc55f240357e7b48c7b18c044c6eb8f0953af8
Zach T <zct8002@rit.edu>
2021-09-07 21:18:30 -0400

    restarted in python flask

--- src/config/settings.js
+++
@@ -1,8 +0,0 @@
-const YAML = require('js-yaml');
-const fs = require('fs');
-
-const settingsName = 'settings.yaml';
-const settingsfile = fs.readFileSync(settingsName);
-const settings = YAML.load(settingsfile)['settings'];
-
-exports.settings = settings;
--- src/config/tmdb-api.js
+++
@@ -1,7 +0,0 @@
-const settings = require('./config/settings').settings;
-const TMDB = require('moviedb');
-const ts = settings['tmdb'];
-
-const mdb = TMDB(ts['key']);
-
-exports.mdb = mdb;
